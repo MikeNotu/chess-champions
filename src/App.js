5663f14556b00432610db70375733705708ff92e
MikeNotu <mikebateman98@gmail.com>
2021-07-15 01:42:14 -0500

    Table Fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import './App.css';
-import {NavBar} from './components/NavBar'
+import "./App.css"
+import { NavBar } from "./components/NavBar"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Table1 } from './components/Table1';
+import { Table1 } from "./components/Table1"
 
-
-function App() {
+function App(props) {
   return (
     <div className="App">
-            <NavBar/>
-            <Table1/>
-
+      <NavBar />
+      <br />
       <h1>Chess Champions Website</h1>
+      <br />
+
+      <div className="container ">
+        <div className="row justify-content-around">
+          <div className="col-sm marginSidesClass">
+            <Table1 />
+          </div>
+          <div className="col-sm marginSidesClass">
+            <Table1 />
+          </div>
+        </div>
+      </div>
+      <div className="container ">
+        <br />
+
+        <div className="row justify-content-around">
+          <div className="col-sm marginSidesClass">
+            <Table1 />
+            <br />
+          </div>
+          <div className="col-sm marginSidesClass counter-position">
+            <Table1 />
+            <br />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Table1.js
+++ src/components/Table1.js
@@ -1,47 +1,164 @@
 import MaterialTable from "material-table"
-import React, {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Modal from "@material-ui/core/Modal"
+import Backdrop from "@material-ui/core/Backdrop"
+import Fade from "@material-ui/core/Fade"
 
-export const Table1 = () =>{
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    // display: "flex",
+    // alignItems: "center",
+    // justifyContent: "center",
+    position: "relative",
+    top: "15% !important",
+    left: "20% !important",
+    width: "60%",
+    height: "7px",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  red: {
+    // position: "relative",
+    // width: "80%",
+    // left: "30%",
+  },
+}))
 
-    const [data, setData] = useState([])
+export const Table1 = () => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
 
-    const columns = [
-        {title:"Name", field: "name"},
-        {title:"Email", field: "email"},
-    ]
+  const handleOpen = () => {
+    setOpen(true)
+    console.log(sr)
+  }
 
-    useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((resp)=>resp.json())
-        .then((resp)=>{
-            setData(resp)
-        })
-    },[])
+  const handleClose = () => {
+    setOpen(false)
+  }
 
+  const [data, setData] = useState([])
+  const [sr, setSr] = useState([])
 
+  const columns = [
+    {
+      field: "avatar",
+      title: "Avatar",
+      render: (rowData) => (
+        <img
+          src={rowData.avatar}
+          style={{ width: 50, borderRadius: "50%" }}
+          alt="user_avatar"
+        />
+      ),
+    },
+    { title: "Rank", field: "rank" },
+    { title: "Username", field: "username" },
+    { title: "Score", field: "score" },
+  ]
 
-    return(
-        <div className="size">
-    <MaterialTable
-        title="Usuarios"
+  useEffect(() => {
+    fetch("https://api.chess.com/pub/leaderboards")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setData(resp.live_blitz)
+        console.log(resp.live_blitz)
+      })
+  }, [])
+
+  function toggleModalWithData(selectedRow) {
+    console.log(selectedRow)
+    setSr(selectedRow)
+    handleOpen()
+  }
+
+  return (
+    <div className="size">
+      <MaterialTable
+        title="Chess.com Blitz Ranking"
         data={data}
         columns={columns}
+        onRowClick={(evt, selectedRow) => toggleModalWithData(selectedRow)}
         options={{
           search: false,
-          paging: true,
+          paging: false,
           filtering: true,
           exportButton: true,
-          actionsColumnIndex: -1,
-        }}
-        localization={{
-          body: {
-            editRow: {
-              deleteText: "¿Esta seguro que desea eliminar a este usuario?",
-            },
-          },
+          //   actionsColumnIndex: -1,
+          maxBodyHeight: "30em",
+          pageSize: 10,
+          pageSizeOptions: [10, 25, 50],
         }}
 
+        // localization={{
+        //   body: {
+        //     editRow: {
+        //       deleteText: "¿Esta seguro que desea eliminar a este usuario?",
+        //     },
+        //   },
+        // }}
       />
-        </div>
-    )
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        // className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h3>
+              {" "}
+              <i className="fas fa-times close" onClick={handleClose}></i>
+            </h3>
+            <div className="container reverse-br">
+              <div className="row justify-content-around">
+                <div className="col-sm marginSidesClass">
+                  <img
+                    className="modal-smallimage "
+                    src={sr.avatar}
+                    alt="HID"
+                  ></img>{" "}
+                </div>
+                <div className="col-sm marginSidesClass">
+                  <h6>
+                    <i className="fas fa-chess-king big-br"></i>&nbsp;Name:{" "}
+                    {sr.name}
+                  </h6>
+                  <h6>
+                    <i className="fas fa-chess-queen"></i>&nbsp;User:{" "}
+                    {sr.username}
+                  </h6>
+                  <h6>
+                    <i className="fas fa-chess-rook"></i>&nbsp;&nbsp;Title:{" "}
+                    {sr.title}
+                  </h6>
+                  <h6>
+                    {" "}
+                    <i className="fas fa-chess-bishop"></i>
+                    &nbsp;&nbsp;&nbsp;Score: {sr.score}
+                  </h6>
+                  <h6>
+                    <i className="fas fa-chess-knight"></i>
+                    &nbsp;&nbsp;&nbsp;Rank: {sr.rank}
+                  </h6>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  )
 }
