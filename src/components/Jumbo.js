bf65eb757ed7599e2e03ae1b998397d08ab77bbd
MikeNotu <mikebateman98@gmail.com>
2021-07-16 00:05:24 -0500

    Fixed Typos

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -4,9 +4,9 @@ export const Jumbo = () => {
   return (
     <>
       <div>
-        <h6 className="display-6">Chess Champions Webiste!</h6>
+        <h6 className="display-6">Chess Champions Website!</h6>
         <h5 className="lead">
-          This is a simple webiste I created to show some of my React +
+          This is a simple website I created to show some of my React +
           Bootstrap skills.
         </h5>
       </div>
